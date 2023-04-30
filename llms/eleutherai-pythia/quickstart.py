2bd885457c2eb7edb97141b16f5e9bf003e9346d
Timo Denk <github@timodenk.com>
2023-04-30 21:12:24 +0200

    Impl. PDG #14 (LLMs)

---
+++ llms/eleutherai-pythia/quickstart.py
@@ -0,0 +1,18 @@
+from transformers import GPTNeoXForCausalLM, AutoTokenizer
+
+model = GPTNeoXForCausalLM.from_pretrained(
+  "EleutherAI/pythia-70m-deduped",
+  revision="step3000",
+  cache_dir="./pythia-70m-deduped/step3000",
+)
+
+tokenizer = AutoTokenizer.from_pretrained(
+  "EleutherAI/pythia-70m-deduped",
+  revision="step3000",
+  cache_dir="./pythia-70m-deduped/step3000",
+)
+
+inputs = tokenizer("Hello, I am", return_tensors="pt")
+print(inputs)
+tokens = model.generate(**inputs)
+print(tokenizer.decode(tokens[0]))
